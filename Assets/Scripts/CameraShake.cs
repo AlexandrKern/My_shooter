9fa5394a2bad0553820fc3540e9c45b0f2a4787f
AlexandrKern <137929102+AlexandrKern@users.noreply.github.com>
2025-01-25 21:21:09 +0300

    Add vfx explosion and camera shake

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    private float duration = 0.18f;
+    [SerializeField] private float magnitude = 0.3f;
+
+    private CameraFollow cameraFollow;
+
+    private void Awake()
+    {
+        cameraFollow = GetComponent<CameraFollow>();
+    }
+
+    public IEnumerator Shake()
+    {
+        if (cameraFollow != null)
+            cameraFollow.SetShaking(true);
+
+        Vector3 originalPosition = transform.position;
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.position = new Vector3(originalPosition.x + x, originalPosition.y + y, originalPosition.z);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = originalPosition;
+
+        if (cameraFollow != null)
+            cameraFollow.SetShaking(false);
+    }
+
+}
+
+
+
